133f41c5cb3b3ed40799632221fd74bc2a15a653
Tushar Potdar <123243424+tusharpotdar99@users.noreply.github.com>
2026-06-13 13:11:34 +0530

    Completed Product Detail Page

--- src/main/java/PageObjects/ProductDetailsPage.java
+++ src/main/java/PageObjects/ProductDetailsPage.java
@@ -4,11 +4,16 @@ import Base.BasePage;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import java.util.List;
 
 public class ProductDetailsPage extends BasePage {
 
     public ProductDetailsPage(WebDriver driver){
         super(driver);
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
     }
 
     @FindBy(xpath = "//ul[@class='breadcrumb']")
@@ -57,6 +62,7 @@ public class ProductDetailsPage extends BasePage {
     WebElement buttonAddtoCart;
 
     @FindBy(xpath = "(//div[@class='image magnific-popup']//a//img)[1]")
+//    @FindBy(css = ".image.magnific-popup img")
     WebElement imageProductImage;
 
     @FindBy(xpath = "//div[@class='image magnific-popup']//div//a")
@@ -77,6 +83,135 @@ public class ProductDetailsPage extends BasePage {
     @FindBy(xpath = "//div[@id='tab-specification']")
     WebElement specificationText;
 
+    @FindBy(xpath = "//input[@id='input-author']")
+    WebElement textReviewerName;
+
+    @FindBy(xpath = "//textarea[@id='input-text']")
+    WebElement textAreaReview;
+
+    @FindBy(xpath = "//div[@id='input-rating']//input[@type='radio']")
+    List<WebElement> radioRating;
+
+    @FindBy(xpath = "//button[@id='button-review']")
+    WebElement buttonReview;
+
+    public String getProductName(){
+        return labelProductName.getText();
+    }
+
+    public String getBrandName(){
+        return labelBrandName.getText();
+    }
+
+    public String getRewardPoints(){
+        return labelRewardPoints.getText();
+    }
+
+    public String getStockAvailability(){
+        return labelProductStockAvailability.getText();
+    }
+
+    public String getProductPrice(){
+        return labelProductPrice.getText();
+    }
+
+    public String getExcludedTaxPrice(){
+        return labelProductPriceExcludedTax.getText();
+    }
+
+    public void clickAddToWishList(){
+        buttonAddToWishList.click();
+    }
+
+    public void clickCompareProduct(){
+        buttonCompareProduct.click();
+    }
+
+    public void enterProductQuantity(String quantity){
+        textProductQuantity.clear();
+        textProductQuantity.sendKeys(quantity);
+    }
+
+    public void clickAddToCart(){
+        buttonAddtoCart.click();
+    }
+
+    public void addProductToCart(String quantity){
+        enterProductQuantity(quantity);
+        clickAddToCart();
+    }
+
+    public boolean isProductImageDisplayed(){
+        return imageProductImage.isDisplayed();
+    }
+
+    public void clickProductImage(){
+        imageProductImage.click();
+    }
+
+    public void clickDescriptionTab(){
+        labelDescriptionNav.click();
+    }
+
+    public void clickSpecificationTab(){
+        labelSpecificationNav.click();
+    }
+
+    public void clickReviewTab(){
+        labelReviewNav.click();
+    }
+
+    public String getDescriptionText(){
+        return descriptionTexts.getText();
+    }
+
+    public String getSpecificationText(){
+        return specificationText.getText();
+    }
+
+    public void enterReviewerName(String name){
+        textReviewerName.sendKeys(name);
+    }
+
+    public void enterReviewText(String review){
+        textAreaReview.sendKeys(review);
+    }
+
+    public void selectRating(int rating){
+        radioRating.get(rating - 1).click();
+    }
+
+    public void clickReviewButton(){
+        buttonReview.click();
+    }
+
+    public void submitReview(
+            String reviewerName,
+            String reviewText,
+            int rating){
+
+        clickReviewTab();
+
+        enterReviewerName(reviewerName);
+
+        enterReviewText(reviewText);
+
+        selectRating(rating);
+
+        clickReviewButton();
+    }
+
+    public boolean isProductPageDisplayed(){
+        return labelProductName.isDisplayed();
+    }
+
+    public boolean isReviewTabDisplayed(){
+        return labelReviewNav.isDisplayed();
+    }
+
+    public String getBreadcrumbText(){
+        return breadcrumb.getText();
+    }
 
 
 
